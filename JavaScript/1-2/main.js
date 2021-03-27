aea49554910efd78a02388f9fbc9b2da68ebd932
matsuse <irotoridorinohiyoko@gmail.com>
2021-03-27 17:24:09 +0900

    課題提出 JavaScript 1-2
    JavaScript 1-2の課題提出です。

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,34 @@
+let name = "hoge";
+//nameと書いたのに、hogeと出力されるはずです
+console.log(name);
+
+// let name = "hoge";
+// console.log(name);
+// let name = "fuga";
+// console.log(name);
+
+// let name = "hoge";
+// console.log(name);
+
+// name = "fuga";
+// console.log(name);
+
+// let name = "池田";
+// console.log(name + "くん");
+// let number = 10;
+// console.log(number * 3);
+
+// console.log(`${name}くんは${number}月生まれです`);
+
+// let name = 'iwasaki';
+// console.log(age);
+
+// let name = 'iwasaki';
+// let name = 'tanaka';
+// console.log(name);
+
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
